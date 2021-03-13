0f05c13a3215b27a79063279e0c0bf6a387b0c22
m4ntr0n1c <jmorel33@hotmail.com>
2021-03-13 18:28:58 -0500

    Update video_modes.c

--- video_modes.c
+++ video_modes.c
@@ -1,6 +1,43 @@
 #include "raylib.h"
 #include <GLFW/glfw3.h>
 
+typedef struct Display_ratio {
+    int width;
+    int height;
+} Display_ratio;
+
+long gcd_l(long a, long b)
+{
+    if (a == 0)
+        return b;
+    else if (b == 0)
+        return a;
+
+    if (a < b)
+        return gcd(a, b % a);
+    else
+        return gcd(b, a % b);
+}
+
+int gcd_i(int a, int b) {
+    int res = a%b;
+    while (res > 0) {
+        a = b;
+        b = res;
+        res = a % b;
+    }
+    return b;
+}
+
+Display_ratio Ratio_info(int x, int y) {
+    Display_ratio p;
+
+    int gcd_res = gcd_i(x,y);
+    p.width = x / gcd_res;
+    p.height = y / gcd_res;
+    return p;
+};
+
 int main()
 {
     InitWindow(1024, 600, "monitor videomodes");
@@ -8,6 +45,7 @@ int main()
     int count;
     GLFWvidmode* modes = glfwGetVideoModes(glfwGetPrimaryMonitor(), &count);
 
+    Display_ratio ratio;
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         BeginDrawing();
@@ -16,13 +54,21 @@ int main()
 
             ClearBackground(RAYWHITE);
             for (int i = 0; i < count; i++) {
-                 DrawText(FormatText("%i x %i rez, %ix%ix%i col, %i rate", (int)modes[i].width, (int)modes[i].height, (int)modes[i].redBits, (int)modes[i].greenBits, (int)modes[i].blueBits, (int)modes[i].refreshRate), x, y, 10, DARKGRAY);
-                 y+=10; if (y>550) {x+=200; y=10; };
-            }
+                ratio = Ratio_info((int)modes[i].width, (int)modes[i].height);
+                
+                DrawText(FormatText("%i x %i rez, %ix%ix%i col, %i rate, ratio = %i:%i",
+                (int)modes[i].width, (int)modes[i].height,
+                (int)modes[i].redBits, (int)modes[i].greenBits, (int)modes[i].blueBits,
+                (int)modes[i].refreshRate, 
+                ratio.width, ratio.height),
+                x, y, 10, DARKGRAY);
+                
+                y+=10; if (y>550) {x+=300; y=10; };
+            };
         EndDrawing();
-    }
+    };
 
     CloseWindow();        // Close window and OpenGL context
 
     return 0;
-}
+};
